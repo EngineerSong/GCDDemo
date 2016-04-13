a03d295a7696fe134695ea0efca8ecb212069852
barara <barara@bararadeimac.lan>
2016-04-13 13:34:27 +0800

    Initial Commit

---
+++ GCDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDDemo
+//
+//  Created by barara on 16/4/13.
+//  Copyright © 2016年 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
